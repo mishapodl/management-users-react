ecb85539faef31f7778fa355e2f7739520249a7e
mishapodl <mishapod@gmail.com>
2020-10-26 11:41:01 +0200

    added redirect to home page in logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import './Logo.scss';
+import { Link } from 'react-router-dom';
 
 export const Logo = () => {
-  return <div className='logo'>Logo</div>;
+  return (
+    <div className='logo'>
+      <Link to='/'>Logo</Link>
+    </div>
+  );
 };
